2419f0bf06977751ac0dbee604f276fa75e702b9
Jeet Viramgama <jviramgama5@gmail.com>
2025-11-29 08:42:03 -0600

    feat: function declaration for ai
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

--- main.py
+++ main.py
@@ -26,14 +26,39 @@ def main():
         types.Content(role="user", parts=[types.Part(text=content)])
     ]
 
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt))
     if '--verbose' in sys.argv:
         print("Verbose mode enabled")
         print(f"User prompt: {content}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    print(response.text)
+    # print(response.text)
+    if response.candidates[0].content.parts:
+        for part in response.candidates[0].content.parts:
+            if part.function_call:
+                print(f"Calling function: {part.function_call.name}({part.function_call.args})")
+
 
 if __name__ == "__main__":
     main()
